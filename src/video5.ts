795460454303bbbd4a736c5b2d83c4989b1e61b6
Fajla Rabby <fajlarabbyla24@gmail.com>
2025-11-12 18:27:41 +0600

    non primitive types

---
+++ src/video5.ts
@@ -0,0 +1,18 @@
+// array , object
+
+// ts - tuple
+
+let bazarList: string[] = ["eggs", "milk", "sugar"];
+
+bazarList.push(true);
+
+let mixedArr: (string | number)[] = ["eggs", 12, "milk", 1, "sugar", 2];
+
+mixedArr.push(true);
+
+// * tuples
+let coordinates: [number, number] = [20, 30, 50];
+
+let couple: [string, string] = ["Husband", "wife"];
+
+let destination: [string, string, number] = ["Dhaka", "Chattogram", 3];
